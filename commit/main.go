26799d0a3e6250b7dac0c500d8b0b70cd67cf606
sunshineplan <sunshineplan@users.noreply.github.com>
2023-02-12 17:21:30 +0800

    Simplify commit

--- commit/main.go
+++ commit/main.go
@@ -1,67 +1,71 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
+	"os"
 	"path/filepath"
 	"strings"
 	"time"
 
-	"github.com/google/go-github/v35/github"
 	"github.com/sunshineplan/database/mongodb"
 	"github.com/sunshineplan/database/mongodb/driver"
 	"github.com/sunshineplan/stock/capitalflows/sector"
-	"golang.org/x/oauth2"
 )
 
-var mongo = driver.Client{
+var client = driver.Client{
 	Database:   "stock",
 	Collection: "capitalflows",
 	Username:   "capitalflows",
 	Password:   "capitalflows",
 	SRV:        true,
 }
-var client mongodb.Client
 
-var token, repository, path string
+var (
+	path = flag.String("path", "", "data path")
+	cmt  = flag.Bool("commit", false, "commit records")
+	del  = flag.Bool("delete", false, "delete records")
+)
+
+var (
+	tz, _ = time.LoadLocation("Asia/Shanghai")
+	now   = time.Now().In(tz)
+	today = now.Format("2006-01-02")
+)
+
+type date struct {
+	Date string `json:"_id" bson:"_id"`
+}
 
 func main() {
-	flag.StringVar(&mongo.Server, "mongo", "", "MongoDB Server")
-	flag.StringVar(&token, "token", "", "token")
-	flag.StringVar(&repository, "repo", "", "repository")
-	flag.StringVar(&path, "path", "", "data path")
+	flag.StringVar(&client.Server, "mongo", "", "MongoDB Server")
 	flag.Parse()
 
-	client = &mongo
 	if err := client.Connect(); err != nil {
 		log.Fatal(err)
 	}
 
-	if err := commit(); err != nil {
+	var res []date
+	if err := client.Aggregate([]mongodb.M{{"$group": mongodb.M{"_id": "$date"}}}, &res); err != nil {
 		log.Fatal(err)
+	}
+
+	if *cmt {
+		if err := commit(res); err != nil {
+			log.Fatal(err)
+		}
+	} else if *del {
+		if err := delete(res); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
-func commit() error {
-	var date []struct {
-		Date string `json:"_id" bson:"_id"`
-	}
-	if err := client.Aggregate([]mongodb.M{{"$group": mongodb.M{"_id": "$date"}}}, &date); err != nil {
-		return err
-	}
-
-	tz, _ := time.LoadLocation("Asia/Shanghai")
-	t := time.Now().In(tz)
-	today := fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
-
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
-
-	for _, i := range date {
+func commit(res []date) error {
+	for _, i := range res {
 		if i.Date != today {
-			res, err := sector.GetTimeLine(i.Date, client)
+			res, err := sector.GetTimeLine(i.Date, &client)
 			if err != nil {
 				return err
 			}
@@ -72,33 +76,29 @@ func commit() error {
 					return err
 				}
 
-				ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-				defer cancel()
-
-				tc := oauth2.NewClient(ctx, ts)
-				client := github.NewClient(tc)
-				repo := strings.Split(repository, "/")
-				fullpath := filepath.Join(append([]string{path}, strings.Split(i.Date, "-")...)...) + ".json"
-				opt := &github.RepositoryContentFileOptions{
-					Message: github.String(i.Date),
-					Content: b,
+				fullpath := filepath.Join(append([]string{*path}, strings.Split(i.Date, "-")...)...) + ".json"
+				if err := os.MkdirAll(filepath.Dir(fullpath), 0755); err != nil {
+					return err
 				}
-
-				if _, _, err := client.Repositories.CreateFile(ctx, repo[0], repo[1], fullpath, opt); err != nil {
-					if !strings.Contains(err.Error(), `"sha" wasn't supplied.`) {
-						return err
-					}
+				if err := os.WriteFile(fullpath, b, 0644); err != nil {
+					return err
 				}
 			}
+		}
+	}
+	return nil
+}
 
+func delete(res []date) error {
+	for _, i := range res {
+		if i.Date != today {
 			d, _ := time.ParseInLocation("2006-01-02", i.Date, tz)
-			if t.Sub(d).Hours() > 7*24 {
+			if now.Sub(d).Hours() > 7*24 {
 				if _, err := client.DeleteMany(mongodb.M{"date": i.Date}); err != nil {
 					return err
 				}
 			}
 		}
 	}
-
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,20 +3,17 @@ module capital-flows
 go 1.19
 
 require (
-	github.com/google/go-github/v35 v35.3.0
 	github.com/sunshineplan/database/mongodb v1.0.5
 	github.com/sunshineplan/database/mongodb/driver v1.0.2
 	github.com/sunshineplan/metadata v1.1.1
 	github.com/sunshineplan/service v1.0.6
 	github.com/sunshineplan/stock v1.0.8
 	github.com/sunshineplan/utils v0.1.17
-	golang.org/x/oauth2 v0.5.0
 )
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/klauspost/compress v1.15.15 // indirect
 	github.com/montanaflynn/stats v0.7.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -33,6 +30,4 @@ require (
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
 )
